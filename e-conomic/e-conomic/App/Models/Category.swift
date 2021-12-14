9c19ec4eb69b9fc3b48c4d34b465d67340f8d28c
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-14 12:19:25 +0100

    Add currency and category resources, and models

---
+++ e-conomic/e-conomic/App/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  e-conomic
+//
+//  Created by iPriyo on 14/12/21.
+//  Copyright © 2021 Priyoneer. All rights reserved.
+//
+
+import Foundation
+
+struct Category: Decodable {
+    let id: Int
+    let name: String
+}
---
+++ e-conomic/e-conomic/App/Models/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  e-conomic
+//
+//  Created by iPriyo on 14/12/21.
+//  Copyright © 2021 Priyoneer. All rights reserved.
+//
+
+import Foundation
+
+
+struct Currency: Decodable {
+    let cc, symbol, name: String
+}
