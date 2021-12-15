839c70ace7cda7856c34f1fb73f75c97cbca00e9
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-15 17:15:50 +0100

    Add test for delete

--- e-conomic/e-conomicTests/Realm/RealmTests.swift
+++ e-conomic/e-conomicTests/Realm/RealmTests.swift
@@ -47,4 +47,60 @@ class RealmTests: XCTestCase {
         
         
     }
+    
+    
+    func testDelete() {
+        
+        let expense = RealmExpense()
+        expense.place_name = "Coop Mega"
+        expense.date = 1639575886303
+        expense.currency_name = "Norwegian Kroner"
+        expense.currency_symbol = "kr."
+        expense.category = "Grocery"
+        expense.amount = 1200
+        expense.receipt_image_name = "receipt_12123123.png"
+        
+        let expense2 = RealmExpense()
+        expense2.place_name = "Coop Mega 1"
+        expense2.date = 1639575886303
+        expense2.currency_name = "Norwegian Kroner"
+        expense2.currency_symbol = "kr."
+        expense2.category = "Grocery"
+        expense2.amount = 1200
+        expense2.receipt_image_name = "receipt_12123123.png"
+        
+        let expense3 = RealmExpense()
+        expense3.place_name = "Coop Mega 2"
+        expense3.date = 1639575886303
+        expense3.currency_name = "Norwegian Kroner"
+        expense3.currency_symbol = "kr."
+        expense3.category = "Grocery"
+        expense3.amount = 1200
+        expense3.receipt_image_name = "receipt_12123123.png"
+
+        let realm = try! Realm()
+        
+        let expenses = realm.objects(RealmExpense.self)
+        XCTAssertEqual(expenses.count, 0)
+
+        try! realm.write {
+            realm.add(expense)
+        }
+        XCTAssertEqual(expenses.count, 1)
+        
+        try! realm.write {
+            realm.add(expense2)
+        }
+        XCTAssertEqual(expenses.count, 2)
+        
+        try! realm.write {
+            realm.add(expense3)
+        }
+        XCTAssertEqual(expenses.count, 3)
+        
+        try! realm.write {
+            realm.delete(expense3)
+        }
+        XCTAssertEqual(expenses.count, 2)
+    }
 }
