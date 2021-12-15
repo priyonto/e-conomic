c2650a5833d09d190359afb5b01f54afea432e23
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-15 09:23:00 +0100

    Add comment to String extension

--- e-conomic/e-conomic/App/Helpers/Extensions/String/String+Ext.swift
+++ e-conomic/e-conomic/App/Helpers/Extensions/String/String+Ext.swift
@@ -8,13 +8,14 @@
 
 import Foundation
 
-
+/// Check option String null or empty
 extension Optional where Wrapped == String {
     var nullOrEmpty: Bool {
         return (self == nil) || ((self?.trimmingCharacters(in: NSCharacterSet.whitespaces).isEmpty) == nil) || (self == "")
     }
 }
 
+/// Convert optional String to optional Int64 and optional Double
 extension Optional where Wrapped == String {
     var int64Value:  Int64? {
         return Int64(self ?? "")
