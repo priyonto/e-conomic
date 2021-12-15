22bd776a5cd4234d8a7b12ed625d36affa393666
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-15 09:24:46 +0100

    Add comment to decodable extension

--- e-conomic/e-conomic/App/Helpers/Extensions/Decodable/Decodable+Ext.swift
+++ e-conomic/e-conomic/App/Helpers/Extensions/Decodable/Decodable+Ext.swift
@@ -8,14 +8,17 @@
 
 import Foundation
 
+// MARK: - Parse Json File
+/// This extension is used to parse local json files into decodable
+/// It works with single object as well as array type
 extension Decodable {
-  static func parse(fileName: String) -> Self? {
-    guard let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
-          let data = try? Data(contentsOf: url),
-          let output = try? JSONDecoder().decode(self, from: data)
+    static func parse(fileName: String) -> Self? {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
+              let data = try? Data(contentsOf: url),
+              let output = try? JSONDecoder().decode(self, from: data)
         else {
-      return nil
+            return nil
+        }
+        return output
     }
-    return output
-  }
 }
