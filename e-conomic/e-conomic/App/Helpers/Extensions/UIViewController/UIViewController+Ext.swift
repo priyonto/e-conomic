1994edb27baf589c285fd43614ef845eb3904e10
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-15 09:42:19 +0100

    Add comments to UIViewController extension

--- e-conomic/e-conomic/App/Helpers/Extensions/UIViewController/UIViewController+Ext.swift
+++ e-conomic/e-conomic/App/Helpers/Extensions/UIViewController/UIViewController+Ext.swift
@@ -8,21 +8,29 @@
 
 import UIKit
 
+// MARK: - Routing
+/// Ideally a UIViewController extension is utilized to navigate to or present different screens
+/// For a small application like this where a lot of independent screens are not available implementing router or coordinator seems overkill and would produce a lot of boilerplate codes
+///
 extension UIViewController {
 
+    /// Navigates to the ExpenseDetailsScreen, required passing an argument of type Expense
+    /// Without an expense object the screen can not be instantiated
     func navigateToExpenseDetailsScreen(_ expense: Expense) {
         let controller = ExpenseDetailsVC(expense)
         navigationController?.pushViewController(controller, animated: true)
     }
     
-    
+    /// Presents expense store screen, required passing an argument of type UIImage
+    /// Without an UIImage object the screen can not be instantiated
     func presentStoreExpenseScreen(_ image: UIImage) {
         let controller = StoreExpenseVC(image)
         let navigationController = UINavigationController(rootViewController: controller)
         present(navigationController, animated: true, completion: nil)
     }
     
-    
+    /// Navigates to the GenericSelectionScreen from where currencies or categories can be selected
+    /// This method takes two argument; SelectionState and reference of GenericSelectionDelegate
     func navigateToGenericSelectionScreen(_ selectionState: SelectionState, delegate: GenericSelectionDelegate) {
         let controller = GenericSelectionVC(selectionState: selectionState, delegate: delegate)
         navigationController?.pushViewController(controller, animated: true)
