d851fea6a45a3ecb1aa041a62af9634125f967b9
Priyonto M Rahman <priyontoo@gmail.com>
2021-12-15 16:29:08 +0100

    Add test cases for initial values on GenericSelectionViewModel

---
+++ e-conomic/e-conomicTests/ViewModels/GenericSelectionViewModelTests.swift
@@ -0,0 +1,28 @@
+//
+//  GenericSelectionViewModelTests.swift
+//  e-conomicTests
+//
+//  Created by iPriyo on 15/12/21.
+//  Copyright © 2021 Priyoneer. All rights reserved.
+//
+
+import XCTest
+@testable import e_conomic
+
+class GenericSelectionViewModelTests: XCTestCase {
+    
+    func testInitWithCurrencySelection() {
+        
+        let viewModel = GenericSelectionViewModel(.currency)
+        XCTAssertEqual(viewModel.currencies.count, 156)
+        
+    }
+    
+    func testInitWithCategorySelection() {
+        
+        var viewModel = GenericSelectionViewModel(.category)
+        XCTAssertEqual(viewModel.categories.count, 8)
+        
+    }
+    
+}
